e0e97b05d8cc964c742f944857ce4c36c019fa6e
julinsantiago <jasantiago84@yahoo.com.br>
2024-03-23 23:22:45 -0300

    Musica do Roupa Nova

---
+++ Musica.py
@@ -0,0 +1,41 @@
+Você lembra, lembra
+Daquele tempo, eu tinha
+Estrelas nos olhos, e um jeito de herói
+Era mais forte e veloz
+Que qualquer mocinho de cowboy
+Você lembra, lembra
+Eu costumava andar bem
+Mais de mil léguas pra poder buscar
+Flores de maio azuis
+E os seus cabelos enfeitar
+Água da fonte cansei de beber
+Pra não envelhecer
+Como quisesse roubar da manhã
+Um lindo pôr de sol
+Hoje não colho mais
+As flores de maio
+Nem sou mais veloz
+Como os heróis
+É, talvez eu seja simplesmente
+Como um sapato velho
+Mas ainda sirvo se você quiser
+Basta você me calçar
+Que eu aqueço o frio dos seus pés
+Água da fonte cansei de beber
+Pra não envelhecer
+Como quisesse roubar da manhã
+Um lindo pôr de sol
+Hoje não colho mais
+As flores de maio
+Nem sou mais veloz
+Como os heróis
+É, talvez eu seja simplesmente
+Como um sapato velho
+Mas ainda sirvo se você quiser
+Basta você me calçar
+Que eu aqueço o frio sos seus pés
+É, talvez eu seja simplesmente
+Como um sapato velho
+Mas ainda sirvo se você quiser
+Basta você me calçar
+Que eu aqueço o frio dos seus pés
--- poesia.py
+++ poesia.py
@@ -2,3 +2,13 @@ Há um menino, há um moleque
 Morando sempre no meu coração
 Toda vez que o adulto balança
 Ele vem pra me dar a mão
+
+Há um passado no meu presente
+Um Sol bem quente lá no meu quintal
+Toda vez que a bruxa me assombra
+O menino me dá a mão
+
+E me fala de coisas bonitas
+Que eu acredito que não deixarão de existir
+Amizade, palavra, respeito
+Caráter, bondade, alegria e amor
