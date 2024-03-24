12c43a9fba644ae5ac800c2f76c975c43eaedfb4
julinsantiago <jasantiago84@yahoo.com.br>
2024-03-23 23:14:42 -0300

    bola de meia bola de gude

--- poesia.py
+++ poesia.py
@@ -1,6 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+Há um menino, há um moleque
+Morando sempre no meu coração
+Toda vez que o adulto balança
+Ele vem pra me dar a mão
